5c9c531e80bf3d6f6005016b05e07b6b367fb470
valgardg <valgardo20@ru.is>
2024-08-21 14:28:56 -0400

    updated: configuration stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  define: {
+      global: {},
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+	optimizeDeps: {
+		exclude: ['.git']
+	},
+	build: {
+		rollupOptions: {
+			input: {
+				main: 'src/main.ts'
+			},
+			external: []
+		},
+		commonjsOptions: {
+			include: /node_modules/,
+		}
+	},
+	server: {
+		watch: {
+			ignored: ['**/.git/**'],
+		}
+	}
+});
