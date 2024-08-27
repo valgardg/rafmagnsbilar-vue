1c69137ccb9d1e077bc4cdd0dfd726df38bfc46e
valgardg <valgardo20@ru.is>
2024-08-27 11:29:51 -0400

    added: vite-svg-loader and updates vite.config.js, logging svg paths in brands component

--- src/components/CarBrands.vue
+++ src/components/CarBrands.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="fw-bold fs-3 mb-4">Vinsæl Bílamerki</div>
-    <div class="mb-5">
-        
-    </div>
+    <img src="@/assets/brands/bmw.svg" />    
 </template>
+
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+// dynamically import all svgs from the brands folder
+const svgModules: Record<string, { default: any}> = import.meta.glob('@/components/brands/*.svg', { eager: true });
+
+// create a reactive object to store svg components with their names
+const svgUrls = reactive<Record<string, any>>({});
+
+for (const path in svgModules) {
+    console.log(path);
+    // const fileName = path.split('/').pop()?.replace('.svg','')
+    // if(fileName) {
+    //     svgUrls[fileName] = svgModules[path];
+    // }
+}
+</script>
