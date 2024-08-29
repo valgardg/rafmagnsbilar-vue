c7bcc2cbf110919f27a12210bfbebe42d901fa56
valgardg <valgardo20@ru.is>
2024-08-29 17:05:16 -0400

    added: about us screen scaffolding and screen component to router index

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeScreen from "../screens/HomeScreen.vue";
+import AboutScreen from "../screens/AboutScreen.vue";
 
 const router = createRouter({
     history: createWebHistory(''),
@@ -12,6 +13,11 @@ const router = createRouter({
             path: '/home',
             name: 'home',
             component: HomeScreen
+        },
+        {
+            path: '/about-us',
+            name: 'about-us',
+            component: AboutScreen
         }
     ]
 })
